e06a1d449f2b33ad66226829de5d8a480f6d2b76
Diomari <diomari.me@gmail.com>
2016-02-21 14:41:32 +0800

    added node modules

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var shell = require('gulp-shell')
+
+gulp.task('serve', function() {
+    browserSync.init({
+        proxy: "localhost:5000",
+        reloadDelay: 2400
+    });
+
+    gulp.watch("content/**/*.lr").on('change', browserSync.reload);
+    gulp.watch("assets/static/styles/*.css").on('change', browserSync.reload);
+    gulp.watch("templates/**/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', shell.task([
+  'lektor server'
+]));
